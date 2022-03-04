4d752fadf1bb39b824f162614c9870efa6a165d6
Julianeme <juliantve@gmail.com>
2022-03-04 12:09:52 -0500

    /api/fileanalyse endpoint added

--- BackEnd_API/boilerplate-project-filemetadata/server.js
+++ BackEnd_API/boilerplate-project-filemetadata/server.js
@@ -1,8 +1,19 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const multer = require('multer')
 
 var app = express();
+
+const storageEngine = multer.diskStorage({
+	  destination: function (req, file, cb) {
+	    cb(null, './uploadedFiles')},
+	  filename: function (req, file, cb) {
+		  cb(null, file.originalname)}
+	})
+
+const upload = multer({ storage: storageEngine })
+
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
@@ -11,7 +22,9 @@ app.get('/', function (req, res) {
     res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post("/api/fileanalyse", upload.single('upfile'), function (req, res) {
+	res.send({ "name": req.file.originalname, "type": req.file.mimetype, "size": req.file.size })
+	});
 
 
 const port = process.env.PORT || 3000;
